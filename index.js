fdf24d20ecb71aba8738d42bc057109b152b2dfb
Ian Walter <public@iankwalter.com>
2018-12-10 23:03:07 -0500

    Allowing dangerous characters because I'm a G

--- index.js
+++ index.js
@@ -1,8 +1,16 @@
-const rehype = require('rehype')
+const unified = require('unified')
 const rehypePrism = require('rehype-prism')
+const parse = require('@starptech/rehype-webparser')
+const toHTML = require('hast-util-to-html')
 require('./javascript')
 
-const processor = rehype().use(rehypePrism)
+function stringify () {
+  function compiler (tree) {
+    return toHTML(tree, { allowDangerousCharacters: true })
+  }
+  this.Compiler = compiler
+}
+const processor = unified().use(parse).use(rehypePrism).use(stringify)
 
 module.exports = function prismLoader (html) {
   return processor.processSync(html).toString()
